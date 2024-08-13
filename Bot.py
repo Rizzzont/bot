3d0a7addbe6fbbccf587e26ea8a5d97e7853b069
Rizzzont <rizzzont@gmail.com>
2024-08-13 22:46:53 +0300

    hi

--- Bot.py
+++ Bot.py
@@ -3,5 +3,7 @@ print("Hello World!")
 for i in range(10):
     print("Hello World!")
 print("Hello World!")
+for i in range(10):
+    print("Hello World!")
 print("Hello World!")
 print("Hello World!")
